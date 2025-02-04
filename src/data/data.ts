05152ed9884512ec8e8daa83d3c2c496ff9d1a0d
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-04 12:17:52 -0300

    Add education icons and improve contact information layout in App component

--- src/data/data.ts
+++ src/data/data.ts
@@ -7,7 +7,7 @@ export type Data = {
 	summary: string;
 	experience: Item[];
 	education: Item[];
-	skills: string[];
+	skills: Skills;
 };
 
 export type Item = {
@@ -20,7 +20,13 @@ export type Item = {
 	activities?: string[];
 };
 
-export type ResumeSections = Array<Item> | string[];
+export type Skills = {
+	technical: string[];
+	tools: string[];
+	softs: string[];
+};
+
+export type ResumeSections = Array<Item> | Skills;
 
 export const data: Data = {
 	name: 'RUBEN ESTEBAN PEÑA PARADA',
@@ -29,7 +35,7 @@ export const data: Data = {
 	email: 'rubenpenap@gmail.com',
 	linkedin: 'linkedin.com/in/rubenpenap/',
 	summary:
-		'Technical leader with experience in frontend development and modern architectures. Specialized in React, TypeScript, and designing scalable solutions, focusing on improving customer experience and fostering high-performance teams.',
+		"I am a seasoned Technical Lead with extensive experience in project management and leading multidisciplinary teams in high-demand environments. I've spearheaded critical initiatives for major clients like Scotiabank Chile, implementing innovative solutions to optimize user experience and strengthen operational resilience. My expertise in strategic planning, continuous improvement, and cross-functional collaboration has driven measurable results, enhanced efficiency, and fostered talent development. I am ready to tackle strategic challenges and deliver sustainable value.",
 	experience: [
 		{
 			id: 'experience-1',
@@ -39,15 +45,10 @@ export const data: Data = {
 			startDate: 'February 2023',
 			endDate: 'Present',
 			activities: [
-				'Led an agile development team, reducing project delivery times by 25% through the implementation of Scrum and TDD methodologies.',
-				'Increased the adoption of technological solutions by 40%, leading digital transformation initiatives aligned with business objectives.',
-				'Designed and implemented modern architectures based on Cloud, APIs, and microservices, improving scalability and operational efficiency.',
-				'Supervised multidisciplinary teams and managed over 3 regional projects simultaneously, achieving 95% stakeholder satisfaction.',
-				'Managed investment and operational budgets, prioritizing key initiatives to maximize return on investment.',
-				'Promoted a culture of continuous improvement through mentorships and the development of technical competencies within the team.',
-				'Implemented innovative solutions that optimized internal banking processes, reducing processing time by 30%.',
-				'Facilitated collaboration between architecture and security teams to ensure the implementation of enterprise standards.',
-				'Organized technical workshops to integrate development practices like TDD, enhancing software quality.',
+				'Led a cell of 5 internal and 3 external developers to enhance the gamification platform, decoupling the offers SDK and cutting load times by 95.5%.',
+				'Developed new offer types by eliminating the SDK, enabling richer BI insights and more effective campaigns.',
+				'Instituted agile processes that improved team communication and ensured on-time, clean releases without rollbacks.',
+				'Spearheaded a regional team to build a mortgage credit simulation tool, automating manual processes and delivering regulatory-compliant insights.',
 			],
 		},
 		{
@@ -58,15 +59,9 @@ export const data: Data = {
 			startDate: 'September 2022',
 			endDate: 'February 2023',
 			activities: [
-				'Led an agile development team, reducing development times by 20% through process optimization and agile methodologies.',
-				'Implemented modern technological solutions, including microservices and microfrontends, increasing efficiency by 30%.',
-				'Fostered effective collaboration between stakeholders and technical teams, ensuring developments aligned with business goals.',
-				'Mentored team members, promoting continuous learning and the adoption of best development practices.',
-				'Supervised the on-time delivery of critical projects, achieving 100% compliance with established deadlines.',
-				'Ensured compliance with security regulations and quality standards at every development stage.',
-				'Introduced modern frameworks to improve code modularity, facilitating maintenance and system scalability.',
-				'Collaborated in the technical definition of strategic projects with architects and business analysts.',
-				'Managed the adoption of DevOps methodologies to enhance continuous integration and delivery workflows.',
+				'Mentored female engineers, boosting their skills and fostering individual and collaborative growth that led to promotions.',
+				"Led the development of the bank's gamification platform, enhancing the loyalty program and customer engagement.",
+				'Collaborated cross-functionally with BI and business teams to drive product improvements and align strategic goals.',
 			],
 		},
 		{
@@ -75,15 +70,9 @@ export const data: Data = {
 			startDate: 'September 2021',
 			endDate: 'September 2022',
 			activities: [
-				'Contributed to the development of scalable technological solutions, optimizing processes and improving user experience by 25%.',
-				'Designed and developed intuitive interfaces, increasing end-user satisfaction by 20%.',
-				'Participated in code reviews, ensuring quality and consistency in each deliverable through best practices like TDD.',
-				'Implemented improvements in development workflows through the adoption of tools and agile methodologies, reducing production defects by 15%.',
-				"Fostered interdisciplinary collaboration between teams, ensuring alignment with the company's strategic objectives.",
-				'Automated repetitive processes through scripts and customized tools, saving the team hours of manual work.',
-				'Reduced technical debt in existing projects by modernizing key components of the technology stack.',
-				'Trained new developers in the use of frameworks and tools adopted by the team.',
-				'Prototyped solutions in early project stages, enabling early validation with stakeholders.',
+				'Contributed to the development and implementation of Apple Pay functionality at Scotiabank Chile, delivering a secure and seamless mobile payment solution.',
+				'Contributed to the internal component library, standardizing development practices across projects.',
+				'Participated in onboarding, training, and recruitment processes, strengthening team capabilities and talent acquisition.',
 			],
 		},
 		{
@@ -92,15 +81,60 @@ export const data: Data = {
 			startDate: 'December 2020',
 			endDate: 'September 2021',
 			activities: [
-				'Developed technological solutions tailored to customer needs, achieving a 10% improvement in application response times.',
-				'Actively participated in creating responsive user experiences, improving accessibility and usability by 15%.',
-				'Applied agile methodologies like Scrum and Kanban to optimize task planning and delivery.',
-				'Contributed to the integration of automated tests, increasing the reliability of developments by 20%.',
-				'Collaborated with multidisciplinary teams to ensure alignment with business objectives and stakeholder expectations.',
-				'Developed functional prototypes to validate requirements, reducing errors in later project phases.',
-				'Proposed improvements to the structure of front-end components, enhancing code performance and maintainability.',
-				'Participated in meetings with stakeholders to understand needs and propose solutions aligned with expectations.',
-				'Maintained updated technical documentation to facilitate onboarding for new developers.',
+				'Developed a microfrontend solution for card blocking and replacement, delivering a modern web experience for Scotiabank Chile.',
+				'Contributed to the first-ever DR cloud exercise, achieving 100% success and reinforcing operational resilience.',
+				'Continuously enhanced functionalities, ensuring optimal system performance and user satisfaction.',
+			],
+		},
+		{
+			id: 'experience-5',
+			name: 'Productos Mitre',
+			title: 'Web Master',
+			location: 'Santiago, Chile',
+			startDate: 'August 2019',
+			endDate: 'Febrero 2020',
+			activities: [
+				'Developed a new e-commerce platform for the brand, significantly enhancing load times and user experience.',
+				'Managed the e-commerce system by uploading standardized product formats and contributing to web content creation.',
+				'Optimized order management by restructuring end-to-end processes, achieving significant reductions in processing times.',
+			],
+		},
+		{
+			id: 'experience-6',
+			name: 'Freelance',
+			title: 'Web Developer',
+			location: 'Venezuela, Colombia, United States and Chile',
+			startDate: 'January 2010',
+			endDate: 'August 2019',
+			activities: [
+				'Built custom e-commerce sites for diverse freelance clients, delivering robust, conversion-focused platforms.',
+				'Developed high-impact landing pages aligned with marketing campaigns to drive targeted engagement.',
+				'Managed ongoing e-commerce operations, including site maintenance and performance optimization.',
+			],
+		},
+		{
+			id: 'experience-7',
+			name: 'Nivel Press',
+			title: 'Web Developer',
+			location: 'Caracas, Venezuela',
+			startDate: 'September 2012',
+			endDate: 'December 2017',
+			activities: [
+				'Built an e-commerce platform for a major Colombian security equipment client, integrating secure payment gateways and optimizing the user experience.',
+				'Developed an e-commerce platform for a new baby products brand that quickly sold out its entire stock within three months.',
+				'Developed high-impact landing pages for various clients, tailored to their marketing campaigns to drive significant engagement.',
+			],
+		},
+		{
+			id: 'experience-8',
+			name: 'Wuaraira Publicidad',
+			title: 'Web Developer',
+			location: 'Caracas, Venezuela',
+			startDate: 'January 2008',
+			endDate: 'December 2011',
+			activities: [
+				'Gained expertise in print production, layout, photography, graphic and web design, utilizing CMS platforms for streamlined content management.',
+				"Developed Papajhon's website with early-generation web technologies, overcoming era-specific challenges to deliver a responsive, user-friendly experience.",
 			],
 		},
 	],
@@ -113,51 +147,56 @@ export const data: Data = {
 			endDate: 'November 2016',
 		},
 	],
-	skills: [
-		'JavaScript (ES6+)',
-		'TypeScript',
-		'HTML5',
-		'CSS3',
-		'React',
-		'Next.js',
-		'React Router',
-		'Zustand',
-		'Redux',
-		'React Query',
-		'TailwindCSS',
-		'Styled Components',
-		'Sass/SCSS',
-		'PostCSS',
-		'CSS Modules',
-		'Vite',
-		'Webpack',
-		'Babel',
-		'ESLint',
-		'Prettier',
-		'Jest',
-		'React Testing Library',
-		'Vitest',
-		'Playwright',
-		'Git',
-		'GitHub',
-		'Bitbucket',
-		'Responsive Design',
-		'Accessibility (WCAG)',
-		'Figma',
-		'Agile Methodologies (Scrum, Kanban)',
-		'Code Reviews',
-		'Component-based Development',
-		'Clean Code',
-		'Mobile-First Development',
-		'Testing Driven Development (TDD)',
-		'REST APIs',
-		'Node.js (for frontend-related tasks)',
-		'NPM/Yarn/PNPM',
-		'Storybook',
-		'Axios',
-		'VS Code',
-		'Technical mentoring for frontend teams',
-		'Collaboration with design and backend teams',
-		'Continuous learning of new technologies',
-	],
+	skills: {
+		technical: [
+			'JavaScript (ES6+)',
+			'TypeScript',
+			'HTML5',
+			'CSS3',
+			'React (Next.js, React Router)',
+			'State Management (Zustand, Redux, React Query)',
+			'Styling (TailwindCSS, Styled Components, Sass/SCSS, PostCSS, CSS Modules)',
+			'Build Tools (Vite, Webpack, Babel)',
+			'Testing (Jest, React Testing Library, Vitest)',
+			'Responsive Design',
+			'Accessibility (WCAG)',
+			'REST APIs',
+			'Frontend con Node.js',
+			'Mobile-First Development',
+			'TDD (Testing Driven Development)',
+		],
+		tools: [
+			'Git',
+			'GitHub',
+			'Bitbucket',
+			'Figma',
+			'Storybook',
+			'Axios',
+			'VS Code',
+			'Chrome DevTools',
+			'Postman',
+			'Jira',
+			'Confluence',
+			'Slack',
+			'Microsoft Teams',
+		],
+		softs: [
+			'Agile Methodologies (Scrum, Kanban)',
+			'Technical Mentorship',
+			'Collaboration',
+			'Cooperation',
+			'Continuous Learning',
+			'Continuous Improvement',
+			'PERT/CPM',
+			'Problem Solving',
+			'Decision Making',
+			'Critical Thinking',
+			'Leadership',
+			'Teamwork',
+			'Adaptability',
+			'Communication',
+			'Time Management',
+			'Planning',
+		],
+	},
 };
